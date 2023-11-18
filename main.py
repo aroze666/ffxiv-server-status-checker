179fa1bfae7bc0a584337f2fda44a31f8ae3ff08
aroze666 <aroze666@gmail.com>
2023-11-17 20:20:04 -0800

    added time

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ while True:
                 if p_name.get_text() == server_name:
                     status = li.find_all("div", class_="world-list__world_category")
                     status = status[0].find_all("p")[0].get_text();
-    print(status)
+    print("%s: %s" % (time.strftime("%H:%M:%S", time.localtime()), status))
     if status != "Congested":
         exit(0)
     time.sleep(60)
